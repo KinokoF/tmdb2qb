13194260f0f12d10fc0cc10ff39636aa5e40afd2
KinokoF <kinokoeffe@outlook.com>
2025-07-12 12:05:10 +0200

    Fix TV alt year extraction

--- src/add-scan/extract.ts
+++ src/add-scan/extract.ts
@@ -28,17 +28,17 @@ export function extractAltYears(
 ): number[] {
   const countries = ["IT", "US", "GB", ...movieOrTv.origin_country];
 
-  const releaseDates = (movieOrTv as RichMovie).release_dates ?? [];
-
-  const years = releaseDates.results
-    .filter((r) => countries.includes(r.iso_3166_1))
-    .flatMap((r) => {
-      const dates = r.release_dates
-        .map((d) => new Date(d.release_date))
-        .sort((a, b) => a.getTime() - b.getTime());
-      const minDate = dates[0];
-      return [minDate.getFullYear(), minDate.getUTCFullYear()];
-    });
+  const years = (movieOrTv as RichMovie).release_dates
+    ? (movieOrTv as RichMovie).release_dates.results
+        .filter((r) => countries.includes(r.iso_3166_1))
+        .flatMap((r) => {
+          const dates = r.release_dates
+            .map((d) => new Date(d.release_date))
+            .sort((a, b) => a.getTime() - b.getTime());
+          const minDate = dates[0];
+          return [minDate.getFullYear(), minDate.getUTCFullYear()];
+        })
+    : [];
 
   const set = new Set([releaseDate.getUTCFullYear(), ...years]);
   set.delete(releaseDate.getFullYear());
